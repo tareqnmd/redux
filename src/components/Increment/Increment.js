6afe537d6dc60c60ae7ae446f034a2723ecea55d
tareqatoffice <tareqatoffice@gmail.com>
2021-11-25 18:15:15 +0600

    redux props

--- src/components/Increment/Increment.js
+++ src/components/Increment/Increment.js
@@ -3,9 +3,9 @@ import { IncrementButton } from '../../styles/Countdown';
 import { connect } from 'react-redux';
 import { increment } from '../../redux/actions/actions';
 
-const Increment = () => {
+const Increment = ({increment}) => {
     return (
-        <IncrementButton onClick={()=>{increment(1)}}>
+        <IncrementButton type="button" onClick={()=>{increment()}}>
             Increment by 1
         </IncrementButton>
     );
