4b16bffe4309ad933aec4280d2dc3fbce7031e6f
tareqatoffice <tareqatoffice@gmail.com>
2021-11-28 10:55:10 +0600

    result updated

--- src/redux/reducers/reducers.js
+++ src/redux/reducers/reducers.js
@@ -15,15 +15,15 @@ const initialState = {
 const reducers = (state = initialState, action) => {
     switch (action.type) {
         case INCREMENT:
-            console.log(value, 'increment');
-            state.value++;
-            localStorage.setItem('value', JSON.stringify(state.value));
-            return state;
+            console.log(state, 'increment');
+            const incrementValue = state.value + 1;
+            localStorage.setItem('value', JSON.stringify(incrementValue));
+            return { ...state, value: incrementValue };
         case DECREMENT:
-            console.log(value, 'decrement');
-            state.value--;
-            localStorage.setItem('value', JSON.stringify(state.value))
-            return state;
+            console.log(state, 'decrement');
+            const decrementValue = state.value - 1;
+            localStorage.setItem('value', JSON.stringify(decrementValue));
+            return { ...state, value: decrementValue };
         default:
             return state;
     }
